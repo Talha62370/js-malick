c1c0aa526813256044423bf7a2d2fcf967f06c0c
Talha62370 <talahaabu830@gmail.com>
2025-04-22 08:07:22 +0000

    03_conversionOperation.js

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "Null"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let isLoggedIn = 1
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log( booleanIsLoggedIn)
+
+// if we write 1 => its give us True; and when 0 => false;
+//  when we take empty string "" => flase,
+// if take value like "Name" => True;
+
+let someNumber = 33
+
+let stringNumber = string(someNumber)
+console.log(stringNumber);
+
+
+
+
