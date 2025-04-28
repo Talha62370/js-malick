cba9dfc256911def54693a29f8bf760bb07e5284
Talha62370 <talahaabu830@gmail.com>
2025-04-28 12:31:20 +0000

    05_strings.js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,39 @@
+const name = "malick"
+const repocount = 50
+
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+const gameName = new String('malick')
+// console.log(gameName[0]);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('l'));
+
+// const newstring = gameName.substring(0, 4)
+// console.log(newstring);
+
+// const anotherstring = gameName.slice(-6, 3)
+// console.log(anotherstring);
+
+// const newstringone = "  malick  " 
+// console.log(newstringone);
+// console.log(newstringone.trim());
+
+// const url = "https://malick.com/malick%20talha"
+// console.log(url.replace('%20', '-'));
+
+// console.log(url.includes('malick'))
+// console.log(url.includes('manjeet'));
+
+const str = "The quick brown fox jumps over the lazy dog.";
+
+const words = str.split(" ");
+console.log(words[3]);
+
+const chars = str.split("");
+console.log(chars[8]);
+
+const strCopy = str.split();
+console.log(strCopy);
