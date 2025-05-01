763ae05160d29beafbed7b9c3e151e2a2c4fa6bc
Talha62370 <talahaabu830@gmail.com>
2025-05-01 11:59:31 +0000

    07_datesinjs.js

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,13 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString);
+// console.log(myDate.toLocaleString);
+// console.log(typeof myDate);
+
+let myCreateDate = new Date(2025, 0, 23, 5, 3)
+console.log(myCreateDate);
+
+let newDate = new Date()
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
